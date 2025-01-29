c4ee2a05185ef51b3e913bf5a3847c6c41a2980f
Emek1212 <abasiemekakpan7@gmail.com>
2025-01-29 01:57:01 -0600

    Updated index.php with correct info

--- index.php
+++ index.php
@@ -1,15 +1,24 @@
 <?php
+
 // Enable CORS
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-// API response data
-$response = [
-    "email" => "abasiemekakpan7@gmail.com", // Replace with your HNG12 Slack email
-    "current_datetime" => gmdate("Y-m-d\TH:i:s\Z"), // Generate current date/time in ISO 8601 (UTC)
-    "github_url" => "git@github.com:Emek1212/hng-task.git" // Replace with your GitHub repo SSH URL
-];
+// Check for the desired endpoint
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+if ($uri == '/hire/php-developers') {
+    // API response data
+    $response = [
+        "email" => "chosennseobong@gmail.com.com",
+        "current_datetime" => gmdate("Y-m-d\TH:i:s\Z"), 
+        "github_url" => "https://github.com/Emek1212/hng-task" 
+    ];
 
-// Send the response as JSON
-http_response_code(200); // HTTP status code for success
-echo json_encode($response);
+    // Send the response as JSON
+    http_response_code(200); // HTTP status code for success
+    echo json_encode($response);
+} else {
+    // If the endpoint doesn't match, return a 404
+    http_response_code(404);
+    echo json_encode(["message" => "Endpoint not found"]);
+}
