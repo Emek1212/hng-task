d0404360452ae2cc14d4680794d63ff6deacbaa0
Emek1212 <abasiemekakpan7@gmail.com>
2025-01-29 03:37:03 -0600

    Updated index.php with correct info

--- index.php
+++ index.php
@@ -1,24 +1,15 @@
 <?php
-
 // Enable CORS
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 
-// Check for the desired endpoint
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-if ($uri == '/hire/php-developers') {
-    // API response data
-    $response = [
-        "email" => "chosennseobong@gmail.com.com",
-        "current_datetime" => gmdate("Y-m-d\TH:i:s\Z"), 
-        "github_url" => "https://github.com/Emek1212/hng-task" 
-    ];
+// API response data
+$response = [
+    "email" => "abasiemekakpan7@gmail.com",
+    "current_datetime" => gmdate("Y-m-d\TH:i:s\Z"), 
+    "github_url" => "git@github.com:Emek1212/hng-task.git" 
+];
 
-    // Send the response as JSON
-    http_response_code(200); // HTTP status code for success
-    echo json_encode($response);
-} else {
-    // If the endpoint doesn't match, return a 404
-    http_response_code(404);
-    echo json_encode(["message" => "Endpoint not found"]);
-}
+
+http_response_code(200); // HTTP status code for success
+echo json_encode($response);
