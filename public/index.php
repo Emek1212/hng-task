6faadbe06a38c79d96b85751175b126bbd59cde0
Emek1212 <abasiemekakpan7@gmail.com>
2025-01-29 12:34:02 -0600

    Added dynamic port handling to index.php in public folder

--- public/index.php
+++ public/index.php
@@ -1,14 +1,5 @@
 <?php
 
-$port = getenv("PORT") ?: 8080; // Use port 8080 if no PORT is set
-
-// Output to verify which port is used
-echo "Server running on port: $port\n";
-
-
-shell_exec("php -S 0.0.0.0:$port -t public");
-
-
 // Enable CORS
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
@@ -17,9 +8,8 @@ header("Content-Type: application/json");
 $response = [
     "email" => "chosennseobong@gmail.com",
     "current_datetime" => gmdate("Y-m-d\TH:i:s\Z"), 
-    "github_url" => "git@github.com:Emek1212/hng-task.git" 
+    "github_url" => "https://github.com/Emek1212/hng-task" // Use HTTPS instead of git@
 ];
-
 
 http_response_code(200); // HTTP status code for success
 echo json_encode($response);
